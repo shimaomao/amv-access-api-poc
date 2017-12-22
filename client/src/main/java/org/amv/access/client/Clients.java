9910dc062180aed6bd30db4c71c9900b2bf6ba84
amvnetworks <development@amv-networks.com>
2017-12-22 13:39:00 +0100

    add missing jackson modules to access api rest client mapper

--- client/src/main/java/org/amv/access/client/Clients.java
+++ client/src/main/java/org/amv/access/client/Clients.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.google.common.annotations.VisibleForTesting;
 import com.netflix.hystrix.*;
@@ -30,6 +32,8 @@ public final class Clients {
     @VisibleForTesting
     public static final ObjectMapper defaultObjectMapper = new ObjectMapper()
             .registerModule(new ParameterNamesModule())
+            .registerModule(new Jdk8Module())
+            .registerModule(new JavaTimeModule())
             .setSerializationInclusion(JsonInclude.Include.NON_NULL)
             .configure(SerializationFeature.INDENT_OUTPUT, true)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
